6619a31621d605a10cd12aa3d0ff879dde1457c2
sakthirithan <sakthirithanofficial@gmail.com>
2026-08-18 22:34:12 +0530

    baseCampy: from project structure to  healthChecker Router first route path

---
+++ projectCamp/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+// Basic configurations
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT","DELETE","PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+// import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("api/v1/healthcheck", healthCheckRouter);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the baseCampy!");
+});
+
+export default app;
---
+++ projectCamp/src/controllers/healthcheck.controllers.js
@@ -0,0 +1,15 @@
+import { ApiResponse } from "../utils/api-response.js";
+
+
+const healthCheck = (req, res) => {
+    try {
+        res.status(200)
+        .json(
+            new ApiResponse(200, {message: "Server is running fine!"}));
+        
+    } catch (err) {
+        
+    }
+};
+
+export { healthCheck };
