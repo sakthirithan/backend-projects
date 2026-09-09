a69c27f4ce9dd74b24b764c60439f3f3a8d5eed7
sakthirithan <itssakthirithan@gmail.com>
2026-09-09 13:57:23 +0530

    feat(model): Hash the password with pre hooks of mongoose

--- projectCamp/src/models/user.models.js
+++ projectCamp/src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from " bcrypt";
 const userSchema = new Schema(
     {
         avatar: {
@@ -59,5 +59,13 @@ const userSchema = new Schema(
         timestamps: true
     },
 );
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+});
+
 
 export const User = mongoose.model("User", userSchema);
